a44abda8228218e316213425fbf7a6aced2cd71a
Ohjay33 <61249083+Ohjay33@users.noreply.github.com>
2020-04-08 13:18:06 -0400

    Add files via upload

--- clas_practice_app.js
+++ clas_practice_app.js
@@ -23,3 +23,36 @@ export default class SignUp extends Component {
       )
    }
 }
+
+class App extends Component {
+    render() {
+      return (
+        <BrowserRouter>
+      
+ {BrowserRouter, Route} from 'react-router-dom';
+          <div>
+            <Route exact={true} path='/' render={() => (
+              <div className="App">
+                <Home />
+              </div>
+            )}/>
+            <Route exact={true} path='/signin' render={() => (
+              <div className="App">
+                <SignIn />
+              </div>
+            )}/>
+            <Route exact={true} path='/signup' render={() => (
+              <div className="App">
+                <SignUp />
+              </div>
+            )}/>
+          </div>
+        </BrowserRouter>
+      );
+    }
+  }
+import Dashboard from './Views/Dashboard';
+import Groups from './Views/Groups';
+import History from './Views/History';
+import Home from './Views/Home';
+import {BrowserRouter, Route} from 'react-router-dom';
